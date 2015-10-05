38e6862eb32fee63fe021dbbcdd8923fedfbb7b7
Daniel Dunning <djdunning1356@gmail.com>
2015-10-05 15:31:35 -0500

    first commit

--- HackerNews/app/src/main/java/group2/hackernews/MainActivity.java
+++ HackerNews/app/src/main/java/group2/hackernews/MainActivity.java
@@ -22,7 +22,11 @@ public class MainActivity extends AppCompatActivity {
 
     String title_url = "https://hacker-news.firebaseio.com/v0/item/";
 
+
     final static String topStories = "https://hacker-news.firebaseio.com/v0/topstories.json";
+    final static String askStories = "https://hacker-news.firebaseio.com/v0/askstories.json";
+    final static String jobStories = "https://hacker-news.firebaseio.com/v0/jobstories.json";
+    final static String newStories = "https://hacker-news.firebaseio.com/v0/jobstories.json";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -116,4 +120,107 @@ public class MainActivity extends AppCompatActivity {
         request.setPriority(Request.Priority.HIGH);
         getter.add(request);
     }
+
+    private void get_jobstories_array() {
+        CustomJSONArrayRequest request = new CustomJSONArrayRequest
+                (Request.Method.GET, jobStories, null, new Response.Listener<JSONArray>() {
+
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        String[] title_id_list = new String[200];
+                        //This is where you get the data from the stored JSON object.
+                        try {
+                            for(int x = 0; x < 200; x++) {
+                                title_id_list[x] = response.getString(x);
+                            }
+                            load_article_title(title_id_list[0]);
+
+                        } catch (Exception e) {
+                            Toast.makeText(MainActivity.this, "ErrorArr", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, "Could not get data", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        request.setPriority(Request.Priority.HIGH);
+        getter.add(request);
+    }
+
+    private void get_askstories_array() {
+        CustomJSONArrayRequest request = new CustomJSONArrayRequest
+                (Request.Method.GET, askStories, null, new Response.Listener<JSONArray>() {
+
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        String[] title_id_list = new String[200];
+                        //This is where you get the data from the stored JSON object.
+                        try {
+                            for(int x = 0; x < 200; x++) {
+                                title_id_list[x] = response.getString(x);
+                            }
+                            load_article_title(title_id_list[0]);
+
+                        } catch (Exception e) {
+                            Toast.makeText(MainActivity.this, "ErrorArr", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, "Could not get data", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        request.setPriority(Request.Priority.HIGH);
+        getter.add(request);
+    }
+
+    private void get_newstories_array() {
+        CustomJSONArrayRequest request = new CustomJSONArrayRequest
+                (Request.Method.GET, newStories, null, new Response.Listener<JSONArray>() {
+
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        String[] title_id_list = new String[500];
+                        //This is where you get the data from the stored JSON object.
+                        try {
+                            for(int x = 0; x < 500; x++) {
+                                title_id_list[x] = response.getString(x);
+                            }
+                            load_article_title(title_id_list[0]);
+
+                        } catch (Exception e) {
+                            Toast.makeText(MainActivity.this, "ErrorArr", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, "Could not get data", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        request.setPriority(Request.Priority.HIGH);
+        getter.add(request);
+    }
+
+ /*   private void filter_page(String filter) {
+        if (filter.equals("job")) {
+            get_jobstories_array();
+        }
+        if (filter.equals("poll")) {
+            get_askstories_array();
+        }
+        if (filter.equals(""))
+    } */
 }
