ddfbb1b25e8051a5a88b3492ddfdb1af59e6f0a3
david <david.deering@ttu.edu>
2015-10-19 08:54:36 -0500

    Modified Story class to use private variables and implemented getter/setter methods.  Changed other classes to use new methods.

--- HackerNews/app/src/main/java/group2/hackernews/Story.java
+++ HackerNews/app/src/main/java/group2/hackernews/Story.java
@@ -5,8 +5,40 @@ package group2.hackernews;
  * These are the properties of the acquired stories
  */
 public class Story {
-    String title;
-    String by;
-    String score;
-    String uri;
+    private String title;
+    private String by;
+    private String score;
+    private String uri;
+
+    public String getTitle(){
+        return title;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public String getBy(){
+        return by;
+    }
+
+    public void setBy(String by){
+        this.by = by;
+    }
+
+    public String getScore(){
+        return score;
+    }
+
+    public void setScore(String score){
+        this.score = score;
+    }
+
+    public String getUri(){
+        return uri;
+    }
+
+    public void setUri(String uri){
+        this.uri = uri;
+    }
 }
